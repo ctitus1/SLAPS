b57b709874f3b726ea1df3a76c5bc4b7a4f8eb81
ctitus1 <titusmchristopher@gmail.com>
2021-09-22 05:17:21 -0400

    Updated with servo positions and servo functions. Drastically reduced memory use (mostly by deleting serial prints) to free up enough space to record current and IMU measurements. Need to finalize code which will be used to determine when to open

--- Editted Code/launch_code_editted/launch_code_editted.ino
+++ Editted Code/launch_code_editted/launch_code_editted.ino
@@ -3,23 +3,26 @@
 #define DETACH_AFTER_MOVING 1 // if 1, servos will detach after moving, if 0, they will not
 #define SD_PIN 4 // pin the SD card is connected to (chip select aka CS pin)
 #define SERVO_PIN 6 // pin the servo is connected to
-#define OPEN_ANGLE 20 // angle for servo to be when open in deg (used to be 50, my servo can't go that far)
-#define CLOSE_ANGLE 100 // angle for servo to be at when closed in deg (used to be 175, my servo can't go that far)
-#define SERVO_DELAY 1000 // time to wait for servo to move in ms
+#define OPEN_ANGLE 110 // angle for servo to be when open in deg (used to be 50, my servo can't go that far)
+#define CLOSE_ANGLE 0 // angle for servo to be at when closed in deg (used to be 175, my servo can't go that far)
+#define DELAY_PER_DEG 11 // delay per degree of movement of servo in ms
+#define DEG_PER_STEP 5 // degrees moved per step of servo
 #define LOOP_DELAY 100 // time to wait between loops in ms
 #define BMP_ADDRESS 0x77 // address for BMP sensor, can be 0x76 or 0x77, but hardware changes are needed (see documentation)
 #define LOCAL_P_MBAR 1022.67 // change to current sea level barrometric pressure (https://www.wunderground.com)
-#define FILENAME "test.txt" // filename of where data is to be written
-#define OPEN_ALTITUDE 20000 // altitude above which the doors will open
+#define FILENAME "test.csv" // Serialname of where data is to be written
+#define OPEN_ALTITUDE 20000 // altitude above which the doors will open in meters
 #define OPEN_TIME 10000 // time after which the door will open in ms regardless of altitude (need to implement regardless of altitude component)
 #define CLOSE_TIME 20000 // time after which the door will close in ms regardless of altitude (need to implement regardless of altitude component)
-#define ERR_LED 8 // pin which serves as output to error LED
+#define FULL_HEADERS 0 // 0 for abbreviated headers in data Serial, 1 for full ones
+#define UNITS 0 // 1 for units in data Serial, 0 for none
+#define HEADERS 1 // 0 headers and units by those specified above, 1 for neither regardless of above specification
 
 // Libraries
 #include <Adafruit_BMP280.h>
 #include <Adafruit_INA219.h>
 #include <Adafruit_Sensor.h>
-//#include <Adafruit_LSM9DS0.h> // IMU library, currently not used in code
+#include <Adafruit_LSM9DS0.h> // IMU library, currently not used in code
 #include <Servo.h>
 #include <SPI.h>
 #include <SD.h>
@@ -27,197 +30,227 @@
 // Objects
 Adafruit_BMP280 bmp;
 Adafruit_INA219 ina;
+Adafruit_LSM9DS0 lsm;
 File file;
 Servo servo;
 
 // Global Variables
 unsigned long current_time;
-enum {default_state, servo_close, servo_opening, servo_open, servo_closing} state =  default_state;
-enum {no_err, bmp_err, sd_err, mem_init_err, mem_write_err} err = no_err;
+enum {default_state, servo_close, servo_opening, servo_open, servo_closing, servo_detached} state =  default_state;
+enum {no_err, bmp_err, sd_err, mem_init_err, mem_write_err, lsm_err} err = no_err;
 float altitude;
 
+// ******** NEED TO ENSURE STATES ARE WORKING CORRECTLY, THEY ARE NOT RN ******** //
 void setup() {
-  pinMode(ERR_LED,OUTPUT);
-  digitalWrite(ERR_LED,err);
-  
+  Serial.begin(9600);
+  Serial.println("Start...");
+
   sensor_init(); // initiates all sensors
-  digitalWrite(ERR_LED,err);
-  
   mem_init(); // creates the data file where data will be stored
-  digitalWrite(ERR_LED,err);
   mem_write(); // takes an initial data point as soon as memory is set up
-  digitalWrite(ERR_LED,err);
-  
-  close_door(0); // closes door ignoring redundancy checks implemented in close_door()
-  digitalWrite(ERR_LED,err);
-  mem_write();
-  digitalWrite(ERR_LED,err);
+  servo.attach(SERVO_PIN);
+  servo.write(CLOSE_ANGLE);
+  state = servo_closing;
+  mem_write(); // takes an initial data point as soon as memory is set up
+  delay(100);
+  servo.detach();
+  state = servo_close;
+  mem_write(); // takes an initial data point as soon as memory is set up
+  for (int i = 0; i < 3; i++) {
+    mem_write();
+    delay(100);
+  }
+  open_door();
+  for (int i = 0; i < 3; i++) {
+    mem_write();
+    delay(100);
+  }
+  close_door();
+  for (int i = 0; i < 3; i++) {
+    mem_write();
+    delay(100);
+  }
+
+  Serial.println("Done!");
+
 }
 
+// ******** NEED TO ENSURE STATES ARE WORKING CORRECTLY, THEY ARE NOT RN ******** //
 void loop() {
-  altitude = bmp.readAltitude(LOCAL_P_MBAR);
-  current_time = millis();
-  if (altitude > OPEN_ALTITUDE || (current_time > OPEN_TIME && current_time < CLOSE_TIME)) {
-    open_door(1);
-    digitalWrite(ERR_LED,err);
-  } else {
-    close_door(1);
-    digitalWrite(ERR_LED,err);
-  }
-  mem_write();
-  digitalWrite(ERR_LED,err);
-  delay(LOOP_DELAY);
+
+  //  altitude = bmp.readAltitude(LOCAL_P_MBAR);
+  //  current_time = millis();
+  //  if (altitude > OPEN_ALTITUDE || (current_time > OPEN_TIME && current_time < CLOSE_TIME)) {
+  //
+  //    open_door();
+  //
+  //  } else {
+  //
+  //    close_door();
+  //
+  //  }
+  //
+  //  mem_write();
+  //  delay(LOOP_DELAY);
+
 }
 
+void close_door() {
 
-void close_door(int use_redunancy_checks) {
-  if (use_redunancy_checks && ( state == servo_close || state == servo_closing )) {
-    return;
-  } else {
-    state = servo_closing;
-    servo.attach(SERVO_PIN);
-    servo.write(CLOSE_ANGLE);
-    mem_write(); // takes data point just before delay for closing servo
-    delay(SERVO_DELAY); // should be replaced with a non-blocking wait
-    state = servo_close;
-    #if DETATCH_AFTER_MOVING
-      servo.detach();
-    #endif
+  servo.attach(SERVO_PIN);
+  state = servo_closing;
+  for (int angle = OPEN_ANGLE; angle >= CLOSE_ANGLE; angle -= DEG_PER_STEP) {
+
+    servo.write(angle); // tell servo to go to specified angle
+    mem_write(); // takes an initial data point as soon as memory is set up
+    delay(DEG_PER_STEP * DELAY_PER_DEG); // waits for the servo to reach the position
+
   }
+
+#if DETATCH_AFTER_MOVING
+  servo.detach();
+#endif
+  state = servo_close;
+  mem_write(); // takes an initial data point as soon as memory is set up
+
 }
 
-void open_door(int use_redunancy_checks) {
-  if (use_redunancy_checks && ( state == servo_open || state == servo_opening )) {
-    return;
-  } else {
-    state = servo_opening;
-    servo.attach(SERVO_PIN);
-    servo.write(OPEN_ANGLE);
-    mem_write(); // takes data point just before delay for opening servo
-    delay(SERVO_DELAY); // should be replaced with a non-blocking wait
-    state = servo_open;
-    #if DETATCH_AFTER_MOVING
-      servo.detach();
-    #endif
+void open_door() {
+
+  servo.attach(SERVO_PIN);
+  state = servo_opening;
+  for (int angle = CLOSE_ANGLE; angle <= OPEN_ANGLE; angle += DEG_PER_STEP) {
+
+    servo.write(angle); // tell servo to go to specified angle
+    mem_write(); // takes an initial data point as soon as memory is set up
+    delay(DEG_PER_STEP * DELAY_PER_DEG); // waits for the servo to reach the position
+
   }
+
+#if DETATCH_AFTER_MOVING
+  servo.detach();
+#endif
+  state = servo_open;
+  mem_write(); // takes an initial data point as soon as memory is set up
+
 }
 
 void sensor_init() {
-  /////////////////////////////////////
-  // Initialize serial communication //
-  /////////////////////////////////////
-  Serial.begin(9600);
-  /////////////////////////////////////
-
   ///////////////////////////
   // Initialize BMP Sensor //
   ///////////////////////////
-  Serial.print("Initializing BMP sensor at address "); Serial.print(BMP_ADDRESS); Serial.print("... ");
-  if (bmp.begin(BMP_ADDRESS)) {
-    Serial.println("success!");
-  } else {
-    Serial.println("initialization failed.");
-    #if HALT_ON_FAILURE // Might want to condense this into a function or macro, works fine for now
-      Serial.println("Program halted.");
-      while (1);
-    #else
-      Serial.println("Program continuing.");
-      err = bmp_err;
-    #endif
+  if (!bmp.begin(BMP_ADDRESS)) {
+
+    err = bmp_err;
+#if HALT_ON_FAILURE
+    while (1);
+#endif
+
   }
   ///////////////////////////
 
   ////////////////////////
   // Initialize SD card //
   ////////////////////////
-  Serial.print("Initializing SD card at pin "); Serial.print(SD_PIN); Serial.print("... ");
   if (!SD.begin(SD_PIN)) {
-    Serial.println("initialization failed.");
-    #if HALT_ON_FAILURE // Might want to condense this into a function or macro, works fine for now
-      Serial.println("Program halted.");
-      while (1);
-    #else
-      Serial.println("Program continuing.");
-      err = sd_err;
-    #endif
+
+    err = mem_write_err;
+#if HALT_ON_FAILURE
+    while (1);
+#endif
+
   }
-  Serial.println("success!");
   ////////////////////////
+
+  ////////////////////
+  // Initialize LSM //
+  ////////////////////
+  if (!lsm.begin()) {
+
+    err = lsm_err;
+#if HALT_ON_FAILURE
+    while (1);
+#endif
+
+  } else {
+
+    // set range/sensitivity of IMU
+    lsm.setupAccel(lsm.LSM9DS0_ACCELRANGE_16G);   // 2G, 4G, 6G, 8G, 16G
+    lsm.setupMag(lsm.LSM9DS0_MAGGAIN_2GAUSS);     // 2GAUSS, 4GAUSS, 8GAUSS
+    lsm.setupGyro(lsm.LSM9DS0_GYROSCALE_2000DPS); // 245DPS, 500DPS, 2000DPS
+
+  }
+  ////////////////////
+
 }
 
 void mem_init() {
-  /////////////////////////////////
-  // Initialize file to write to //
-  /////////////////////////////////
-  String header = "time (ms),err,state,pressure (Pa),temp (C),altitude (m),solar_voltage (V),servo_angle (deg)";
 
-  Serial.print("Opening "); Serial.print(FILENAME); Serial.print("... ");
-  file = SD.open(FILENAME, FILE_WRITE);
+  file = SD.open(FILENAME, FILE_WRITE); // creates and names file
 
   if (file) {
-    Serial.print("success!");
-    Serial.println();
 
-    Serial.print("Writing headers to "); Serial.print(FILENAME); Serial.print("... ");
-    file.println(header);
-    file.close();
+#if HEADERS
+#if FULL_HEADERS
+//  file.println("time,err,state,pressure,temp,altitude,sh_voltage,bu_voltage,current,power,servo_angle,A_x,A_y,A_z,G_x,G_y,G_z"); // full headers
+    file.println("time,err,state,pressure,temp,altitude,sh_voltage,bu_voltage,current,power,servo_angle,A_x,A_y,A_z,G_x,G_y,G_z"); // full headers
+#else
+//  file.println("ti,e,st,pr,te,a,Vs,Vb,c,po,Sa,Ax,Ay,Az,Gx,Gy,Gz"); // short headers
+    file.println("ti,e,st,pr,te,a,Vs,Vb,c,po,Sa,Ax,Ay,Az,Gx,Gy,Gz"); // short headers
+#endif
 
-    Serial.println("success!");
+#if UNITS
+//  file.println("ms,,,Pa,C,m,mV,V,mA,mW,deg,g,g,g,dps,dps,dps"); // units
+    file.println("ms,,,Pa,C,m,mV,V,mA,mW,deg,g,g,g,dps,dps,dps"); // units
+#endif
+#endif
+
+    file.close(); // closes file
+
   } else {
-    Serial.print("opening "); Serial.print(FILENAME); Serial.println(" in mem_init() failed");
-    #if HALT_ON_FAILURE // Might want to condense this into a function or macro, works fine for now
-      Serial.println("Program halted.");
-      while (1);
-    #else
-      Serial.println("Program continuing.");
-      err = mem_init_err;
-    #endif
+
+    err = mem_write_err;
+#if HALT_ON_FAILURE
+    while (1);
+#endif
+
   }
-  /////////////////////////////////
+
 }
 
 void mem_write() {
-  Serial.print("Opening "); Serial.print(FILENAME); Serial.print("... ");
+
   file = SD.open(FILENAME, FILE_WRITE);
-  if (file) {
-    Serial.print("success!");
-    Serial.println();
 
-    float pressure, temp, solar_voltage;
-    int servo_angle;
+  if (file) { // writes data to file
 
-    Serial.print("Measuring data... ");
-    current_time = millis();
-    pressure = bmp.readPressure();
-    temp = bmp.readTemperature();
-    altitude = bmp.readAltitude(LOCAL_P_MBAR);
-    solar_voltage = ina.getBusVoltage_V();
-    servo_angle = servo.read();
-    Serial.print("success!");
-    Serial.println();
+    lsm.read();
 
-    Serial.print("Writing to "); Serial.print(FILENAME); Serial.print("... ");
-    file.print(current_time); file.print(",");
-    file.print(err); file.print(",");
-    file.print(state); file.print(",");
-    file.print(pressure); file.print(",");
-    file.print(temp); file.print(",");
-    file.print(altitude); file.print(",");
-    file.print(solar_voltage); file.print(",");
-    file.print(servo_angle); file.println();
+    file.print(millis()); file.print(",");                        // time
+    file.print(err); file.print(",");                             // err
+    file.print(state); file.print(",");                           // state
+    file.print(bmp.readPressure()); file.print(",");              // pressure
+    file.print(bmp.readTemperature()); file.print(",");           // temp
+    file.print(bmp.readAltitude(LOCAL_P_MBAR)); file.print(",");  // altitude
+    file.print(ina.getShuntVoltage_mV()); file.print(",");        // sh_voltage
+    file.print(ina.getBusVoltage_V()); file.print(",");           // bu_voltage
+    file.print(ina.getCurrent_mA()); file.print(",");             // current
+    file.print(ina.getPower_mW()); file.print(",");               // power
+    file.print(servo.read()); file.print(",");                    // servo_angle
+    file.print(lsm.accelData.x); file.print(",");                 // A_x
+    file.print(lsm.accelData.y); file.print(",");                 // A_y
+    file.print(lsm.accelData.z); file.print(",");                 // A_z
+    file.print(lsm.gyroData.x); file.print(",");                  // G_x
+    file.print(lsm.gyroData.y); file.print(",");                  // G_y
+    file.print(lsm.gyroData.z); file.println();                   // G_z
     file.close();
-    Serial.print("success! File closed.");
-    Serial.println();
 
   } else {
-    Serial.print("opening "); Serial.print(FILENAME); Serial.print(" in mem_write() failed");
-    Serial.println();
-    #if HALT_ON_FAILURE // Might want to condense this into a function or macro, works fine for now
-      Serial.println("Program halted.");
-      while (1);
-    #else
-      Serial.println("Program continuing.");
-      err = mem_write_err;
-    #endif
+
+    err = mem_write_err;
+#if HALT_ON_FAILURE
+    while (1);
+#endif
+
   }
 }
